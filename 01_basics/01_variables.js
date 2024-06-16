4533cc542da95dd4b4426550b71f526e274ea0b4
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2024-06-16 18:29:17 +0530

    16_06_2024

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,23 +1,17 @@
-/*
-1. const used to define constants 
-2. 3 types to declare variables 1. let (most prefrable) 2.var (not prefrable) 3. without any keyword .
-3. if any variable is not initialize with any value it contains undefined by js nature . 
-*/
-const accountId = 144553
-let accountEmail = "saksham@gmail.com"
-var accountPassword = "12345"
-accountCity = "Gwalior"
+// constat declare -> const 
+// variables decalre -> let or var 
+
+const accountId = 144553;
+let accountEmail = "saksham@google.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
 let accountState;
 
-//accountId = 2 // not allowed
 
+// accountId = 2; not allowed 
+accountEmail = "sa@gamilc.com"
 console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
 
-accountEmail = "sak@gmail23.com"
-accountPassword = "1212121"
-accountCity = "beglau"
-/* 
-Prefer not to use var 
-because of issue in block scope and functional scope 
-*/
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+// Prefer not to use var because of issue in block scope and functional scope . 
+ 
